86bf70596554c9135d4de364b875be2773aa0a35
lkleijn <louis.kleijn95@gmail.com>
2016-10-19 09:43:13 +0200

    Create AdvertController.php

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,20 @@
+<?php
+
+// src/OC/PlatformBundle/Controller/AdvertController.php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdvertController extends Controller
+{
+    // On récupère tous les paramètres en arguments de la méthode
+    public function viewSlugAction($slug, $year, $format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '".$slug."', créée en ".$year." et au format ".$format."."
+        );
+    }
+}
