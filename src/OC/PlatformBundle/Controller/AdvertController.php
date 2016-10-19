a30db30ccce4f7bd5a0dcc7713e776d165097d00
lkleijn <louis.kleijn95@gmail.com>
2016-10-19 17:21:00 +0200

    modif routing

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,26 +5,37 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdvertController extends Controller
 {
   // …
 
- public function viewAction($id)
+   public function viewAction($id)
 
   {
 
-    // On utilise le raccourci : il crée un objet Response
+    return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
 
-    // Et lui donne comme contenu le contenu du template
+      'id' => $id
 
-    return $this->render(
-
-      'OCPlatformBundle:Advert:view.html.twig',
-
-      array('id'  => $id));
+    ));
 
   }
-}
+  
+  public function addAction(Request $request)
+  {
+    $session = $request->getSession();
+    // Bien sûr, cette méthode devra réellement ajouter l'annonce
+   // Mais faisons comme si c'était le cas
+    $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+    // Le « flashBag » est ce qui contient les messages flash dans la session
+    // Il peut bien sûr contenir plusieurs messages :
+    $session->getFlashBag()->add('info', 'Oui oui, elle est bien enregistrée !');
+    // Puis on redirige vers la page de visualisation de cette annonce
+    return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+  }
+
+  
+  }
