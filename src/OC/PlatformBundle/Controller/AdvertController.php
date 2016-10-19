0874a1a46021fa7c113cf62befd947f2c7117a40
lkleijn <louis.kleijn95@gmail.com>
2016-10-19 15:53:02 +0200

    modif controller

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,16 +5,26 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
-    // On récupère tous les paramètres en arguments de la méthode
-    public function viewSlugAction($slug, $year, $format)
-    {
-        return new Response(
-            "On pourrait afficher l'annonce correspondant au
-            slug '".$slug."', créée en ".$year." et au format ".$format."."
-        );
-    }
+  // …
+
+ public function viewAction($id)
+
+  {
+
+    // On utilise le raccourci : il crée un objet Response
+
+    // Et lui donne comme contenu le contenu du template
+
+    return $this->render(
+
+      'OCPlatformBundle:Advert:view.html.twig',
+
+      array('id'  => $id));
+
+  }
 }
